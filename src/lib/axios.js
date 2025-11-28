2528b7b2ddcc548ac929541a1901a03d6be1df3c
kumarnivi <nivethikashivakumar56@gmail.com>
2025-11-28 10:12:24 +0530

    change base url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "https://videocall-chat-api.onrender.com/api" : "/api";
+// const BASE_URL = import.meta.env.MODE === "development" ? "https://videocall-chat-api.onrender.com/api" : "/api";
+
+const BASE_URL = "https://videocall-chat-api.onrender.com/api";
+
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
