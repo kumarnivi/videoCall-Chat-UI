d88e2b574f77705263b43406f6f5b993ee6ea6c5
kumarnivi <nivethikashivakumar56@gmail.com>
2025-11-30 19:09:51 +0530

    URL chnageed

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-// const BASE_URL = import.meta.env.MODE === "development" ? "https://videocall-chat-api.onrender.com/api" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://videocall-chat-api.onrender.com/api";
 
-const BASE_URL = "https://videocall-chat-api.onrender.com/api";
+// const BASE_URL = "https://videocall-chat-api.onrender.com/api";
 
 
 export const axiosInstance = axios.create({
